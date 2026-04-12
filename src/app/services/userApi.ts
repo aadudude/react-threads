9e9a4397ec02b587b1ac261ffc7b258384c5bcce
Aleksei <alekseia1559@gmail.com>
2026-04-12 17:38:07 +0300

    feat: настроить userApi

--- src/app/services/userApi.ts
+++ src/app/services/userApi.ts
@@ -1,9 +1,28 @@
 import {api} from "./api";
-import {build} from "vite";
+import type {LoginProps, RegisterProps} from "./types.ts";
+import type {User} from "../types.ts";
 
 
 export const userApi = api.injectEndpoints({
 endpoints:(build)=>({
-    login: build.mutation<{token:string}>
+    register: build.mutation<User,RegisterProps>({
+        query:({email,password,name})=>({
+            url:'/api/register',
+            method:'POST',
+            body: {email,password,name}
+        })
+    }),
+    login: build.mutation<User,LoginProps>({
+        query:({email,password})=>({
+            url:'/api/login',
+            method: 'POST',
+            body:{email,password}
+        })
+    }),
+    current:build.query<User,void>({
+        query:()=>({url:'/api/current'})
+    })
 })
 })
+
+export const {useRegisterMutation,useLoginMutation,useCurrentQuery} = userApi
