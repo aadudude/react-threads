5e1df5578c183fec8505603385ebc8b175eeb814
Aleksei <alekseia1559@gmail.com>
2026-04-15 15:38:04 +0300

    feat: теги для эндпоинтов-2

--- src/app/services/commentsApi.ts
+++ src/app/services/commentsApi.ts
@@ -1,23 +1,29 @@
-import {api} from "./api.ts";
-import type {CommentProps, CommentRequestProps} from "./types.ts";
+import { api } from "./api.ts"
+import type { CommentProps, CommentRequestProps } from "./types.ts"
 
 
 export const commentsApi = api.injectEndpoints({
-    endpoints: (build) => ({
-        createComment: build.mutation<CommentProps, CommentRequestProps>({
-            query: ({content, postId}) => ({
-                url: '/comments',
-                method: 'POST',
-                body: {content, postId}
-            })
-        }),
-        deleteComment: build.mutation<void, { id: string }>({
-            query: ({id}) => ({
-                url: `/comments/${id}`,
-                method: 'DELETE'
-            })
-        })
-    })
+  endpoints: (build) => ({
+    createComment: build.mutation<CommentProps, CommentRequestProps>({
+      query: ({ content, postId }) => ({
+        url: "/comments",
+        method: "POST",
+        body: { content, postId },
+      }),
+      invalidatesTags:(_result,_error,arg) => [
+        { type:"Post",id:arg.postId },
+      ],
+    }),
+    deleteComment: build.mutation<void, { postId: string, commentId: string }>({
+      query: ({ commentId }) => ({
+        url: `/comments/${commentId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags:(_result,_error,arg) => [
+        { type:"Post",id:arg.postId },
+      ],
+    }),
+  }),
 })
 
-export const {useCreateCommentMutation,useDeleteCommentMutation} = commentsApi
+export const { useCreateCommentMutation, useDeleteCommentMutation } = commentsApi
