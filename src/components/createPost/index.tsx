50936f9d4f984bfed44328c3df53e98a730c2491
Aleksei <alekseia1559@gmail.com>
2026-04-15 10:46:26 +0300

    feat: доработать CreatePost перевести на RHF

--- src/components/createPost/index.tsx
+++ src/components/createPost/index.tsx
@@ -1,55 +1,73 @@
-import { Button, Description, TextArea } from "@heroui/react"
+import { Button, Description, ErrorMessage, FieldError, Form, Label, TextArea, TextField } from "@heroui/react"
 import { useState } from "react"
 import { LogoTelegram } from "@gravity-ui/icons"
 import { useCreatePostMutation, useLazyGetPostsQuery } from "../../app/services/postsApi.ts"
 import { isErrorWithMessage, isFetchBaseQueryError } from "../../services/helpers.ts"
+import { useForm } from "react-hook-form"
+
+type PostForm = {
+    content: string
+}
 
 export const CreatePost = () => {
   const [createPost, { isLoading }] = useCreatePostMutation()
   const [triggerGetPosts] = useLazyGetPostsQuery()
 
-  const [value, setValue] = useState("")
-  const [error, setError] = useState("")
+  const { register, handleSubmit, formState: { errors, isValid },watch } = useForm<PostForm>({
+    mode: "onTouched",
+    reValidateMode: "onBlur",
+  })
+  const contentValue = watch("content") || ""
 
-  const handleCreatePost = async () => {
-    try{
-      await createPost({ content: value }).unwrap()
-      setValue("")
+  const [fetchError, setFetchError] = useState("")
+
+  const onSubmit = async () => {
+    try {
+      await createPost({ content: contentValue }).unwrap()
       await triggerGetPosts()
-    }catch (err){
+      setFetchError("")
+    } catch (err) {
       if (isFetchBaseQueryError(err)) {
         const errMsg = "error" in err ? err.error : (err.data as {
-                error?: string
-            })?.error || "Что-то пошло не так"
-        setError(errMsg)
+                    error?: string
+                })?.error || "Что-то пошло не так"
+        setFetchError(errMsg)
       } else if (isErrorWithMessage(err)) {
-        setError(err.message)
+        setFetchError(err.message)
       }
     }
-
   }
-
   return (
     <div className='flex flex-col gap-6'>
-      <div className="flex flex-col gap-2">
-        <TextArea
-          required
+      <Form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
+
+        <TextField isRequired isInvalid={!!errors.content} name="bio">
+          <Label>Bio</Label>
+          <TextArea {...register("content", {
+            required: "Напишите что-то",
+            minLength: {
+              value: 5,
+              message: "Пост не может быть меньше 5 символов",
+            },
+          })}
           className="h-32"
           aria-describedby="textarea-controlled-description"
           aria-label="Announcement"
-          placeholder="О чем думаете?"
-          value={value}
-          onChange={(event) => setValue(event.target.value)}
-        />
+          placeholder="О чем думаете?"/>
+          {errors.content && (
+            <FieldError>{errors.content.message}</FieldError>
+          )}
+        </TextField>
+
         <Description id="textarea-controlled-description">
-                    Символов: {value.length} / 280
+                        Символов: {contentValue.length} / 280
         </Description>
-      </div>
-      {error && <p>error</p> }
-      <Button onClick={handleCreatePost} isDisabled={isLoading}>
-                Добавить пост
-        <LogoTelegram/>
-      </Button>
+        <ErrorMessage>{!!fetchError && <>{fetchError}</>}</ErrorMessage>
+        <Button type='submit' isDisabled={ !isValid || isLoading}>
+                    Добавить пост
+          <LogoTelegram/>
+        </Button>
+      </Form>
     </div>
   )
 }
